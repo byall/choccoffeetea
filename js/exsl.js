7f8c2580e275409101da515b98269720ed6a7fa4
alliswell <talbymisty@aol.com>
2016-07-26 21:11:26 +0000

    Next Draft - Tuesday

--- js/exsl.js
+++ js/exsl.js
@@ -1,11 +1,12 @@
-// JavaScript File
+// JavaScript File 
+
 $(document).ready(function(){
 $('#move').click(function(){
 $('#move').effect('slide',{direction:'right'}, 500);
 });
-});
+}); 
 $(document).ready(function(){
-$('#ex').click(function(){
-$('#ex').effect('explode', {pieces: 4}, 7000);
+$('#explode').click(function(){
+$('#explode').effect('explode', {pieces: 9}, 7000);
 });
 });
