2f8b9240a7a6b1548aca88043679e6668bf0b264
alliswell <talbymisty@aol.com>
2016-07-26 01:12:16 +0000

    Still Needs Work

--- js/game.js
+++ js/game.js
@@ -11,8 +11,8 @@ function startGame() {
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 880;
-        this.canvas.height = 500;
+        this.canvas.width = 680;
+        this.canvas.height = 400;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
